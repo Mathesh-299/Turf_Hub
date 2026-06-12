399bee45e593a071ff2b841c34442c61587781ea
Mathesh-299 <postbox2992005@gmail.com>
2026-06-12 15:18:36 +0530

    Update payment route

--- turf_server/routes/payment.js
+++ turf_server/routes/payment.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createOrder, verifyPayment } = require("../controller/paymentController");
+const { createOrder, verifyPayment, handleWebhook } = require("../controller/paymentController");
 const { authenticateJWT } = require("../middleware/authentication");
 const router = express.Router();
 
 router.post("/createOrder", authenticateJWT, createOrder);
+router.post("/create-order", authenticateJWT, createOrder);
 router.post("/verifyPayment", authenticateJWT, verifyPayment);
+router.post("/verify-payment", authenticateJWT, verifyPayment);
+router.post("/webhook", handleWebhook);
 
 module.exports = router;
